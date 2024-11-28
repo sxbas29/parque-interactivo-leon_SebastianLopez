f8ac7b3c0c6b1bb6a7371b5edb150824883c5f40
sexbax <194284-7@iberoleon.edu.mx>
2024-11-28 11:55:10 -0600

    Update atracciones.py
    E4: Fin del examen

--- atracciones.py
+++ atracciones.py
@@ -1,2 +1,3 @@
 atracciones_antiguas = ['Rueda de la Fortuna', 'Caballitos']
 atracciones_modernas = ['Simulador Virtual', 'Carrusel Moderno']
+atracciones = ['Carros chocones', 'Barco pirata', 'Sillas voladoras']
